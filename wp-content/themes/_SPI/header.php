cd80dd5d302fd95db62490422482579c036d6373
sarahholden <sarahbeth.holden@gmail.com>
2014-08-19 22:10:44 -0500

    Fixed top-nav styles

--- wp-content/themes/_SPI/header.php
+++ wp-content/themes/_SPI/header.php
@@ -57,12 +57,12 @@
 					<?php // bloginfo('description'); ?>
 
 
-					<nav role="navigation">
+					<nav role="navigation" class="top-nav">
 						<?php wp_nav_menu(array(
     					'container' => false,                           // remove nav container
     					'container_class' => 'menu cf',                 // class of container (should you choose to use it)
     					'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					'menu_class' => 'nav top-nav cf',               // adding custom nav class
+    					'menu_class' => 'nav cf',               // adding custom nav class
     					'theme_location' => 'main-nav',                 // where it's located in the theme
     					'before' => '',                                 // before the menu
         			'after' => '',                                  // after the menu
