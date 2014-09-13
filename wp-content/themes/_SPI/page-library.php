d96b7068aa1a355da9f9ad1d1ff505a6ca710554
sarahholden <sarahbeth.holden@gmail.com>
2014-09-13 12:21:05 -0600

    Library continued

--- wp-content/themes/_SPI/page-library.php
+++ wp-content/themes/_SPI/page-library.php
@@ -7,6 +7,17 @@ $context = Timber::get_context();
 $post = new TimberPost();
 $context['post'] = $post;
 
+$library_section_fields = $post->get_field('library_sections');
+
+$library_sections = array();
+
+foreach ($library_section_fields as $section){
+    $library_sections[] = new TimberPost($section['library_section']->ID);
+}
+
+$context['library_sections'] = $library_sections;
+
 Timber::render('library.twig', $context);
+
 
 ?>
