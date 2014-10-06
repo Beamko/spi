048183612538de68bda1f5f8fd06023d16f5b1cd
sarahholden <sarahbeth.holden@gmail.com>
2014-10-06 16:28:04 -0500

    Fix for newsletter

--- wp-content/themes/_SPI/library/js/scripts.js
+++ wp-content/themes/_SPI/library/js/scripts.js
@@ -207,39 +207,37 @@ jQuery(document).ready(function($) {
   }
 
   spi.newsletterSignup = function () {
-    $('.js-submit-newsletter-1').click(function (e) {
+    $('.js-open-newsletter-modal').click(function (e) {
+      e.preventDefault();
+      // _gaq.push(['_trackEvent', 'footer', 'open-newsletter-sign-up']);
+
+      $('.js-newsletter-form').slideToggle('fast');
+
+    });
+
+    $('.js-submit-newsletter').on('click', function (e) {
       e.preventDefault();
       // _gaq.push(['_trackEvent', 'footer', 'newsletter-sign-up']);
 
-      var form = $('#newsletter-signup-1');
-      var formEmail = form.find('#email-address');
+      var form = $('#mc-embedded-subscribe-form');
+      var formEmail = form.find('#mce-EMAIL');
       var emailVal = formEmail.val();
       var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
       formEmail.removeClass('invalid');
       form.find('.error').remove();
 
-
-
       if (emailVal == '') {
         formEmail.addClass('invalid');
         formEmail.focus();
-        formEmail.after("<div class='error'>Please enter your email.</div>");
+        formEmail.after("<div class='error error-message'>Please enter your email.</div>");
       } else if (!regex.test(emailVal)) {
         formEmail.addClass('invalid');
         formEmail.focus();
-        formEmail.after("<div class='error'>Please enter a valid email address.</div>");
+        formEmail.after("<div class='error error-message'>Please enter a valid email address.</div>");
       } else {
-        $('.js-newsletter-form').slideToggle('fast');
-        $('.js-newsletter-form').find('#mce-EMAIL').val(emailVal).attr('disabled', true);
-
-        $('#mc-embedded-subscribe').on('click', function () {
-          $('.js-newsletter-form').find('#mce-EMAIL').attr('disabled', false);
-        })
+        form.submit();
       }
-
-      
-
     });
 
     $('.js-newsletter-form a').click(function (e) {
